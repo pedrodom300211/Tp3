e87482aa3762c4740872957635db9f2622509156
Mateo Coronel <mateocoronel240@gmail.com>
2023-04-13 09:50:18 -0300

    Avance 6 -> Mateo Coronel, Validaciones localidad

--- TrabajoPractico3/Ejercicio1.aspx.cs
+++ TrabajoPractico3/Ejercicio1.aspx.cs
@@ -11,7 +11,27 @@ namespace TrabajoPractico3
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            
+        }
 
+        protected void btnGuardarLocalidad_Click(object sender, EventArgs e)
+        {
+            Boolean localidadRepetida = true;
+            for (int i = 0; i < ddlLocalidades.Items.Count; i++) {
+                if (txtLocalidades0.Text.Trim().ToUpper() == ddlLocalidades.Items[i].Text.Trim().ToUpper())
+                {
+                    localidadRepetida = false;
+                    string script = "alert('Localidad repetida');"; //Se guarda un script utilizando una funcion llamada "alert" pero del lenguaje de JavaScript
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", script, true);
+                    // Aca se esta registrando un script en el lado del servidor utilizando el objeto ScriptManager de ASP.NET, que muestra una ventana emergente con el mensaje
+                    //Al agregar el segundo parámetro "this.GetType()" a la función RegisterStartupScript,
+                    //nos aseguramos de que el script solo se registre una vez por página 
+                }
+            }
+            if (localidadRepetida == true) {
+                ddlLocalidades.Items.Add(txtLocalidades0.Text);
+                txtLocalidades0.Text = ""; 
+            }
         }
     }
 }
--- TrabajoPractico3/Ejercicio1.aspx.designer.cs
+++ TrabajoPractico3/Ejercicio1.aspx.designer.cs
@@ -24,13 +24,22 @@ namespace TrabajoPractico3
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// Control txtLocalidades.
+        /// Control txtLocalidades0.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtLocalidades;
+        protected global::System.Web.UI.WebControls.TextBox txtLocalidades0;
+
+        /// <summary>
+        /// Control rfvLocalidades.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvLocalidades;
 
         /// <summary>
         /// Control btnGuardarLocalidad.
@@ -40,5 +49,86 @@ namespace TrabajoPractico3
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnGuardarLocalidad;
+
+        /// <summary>
+        /// Control txtNombreUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombreUsuario;
+
+        /// <summary>
+        /// Control txtContrasenia.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtContrasenia;
+
+        /// <summary>
+        /// Control txtRepContraseña.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtRepContraseña;
+
+        /// <summary>
+        /// Control txtCorreo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCorreo;
+
+        /// <summary>
+        /// Control txtCp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCp;
+
+        /// <summary>
+        /// Control ddlLocalidades.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlLocalidades;
+
+        /// <summary>
+        /// Control btnGUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnGUsuario;
+
+        /// <summary>
+        /// Control lblBienvenida.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblBienvenida;
+
+        /// <summary>
+        /// Control Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
     }
 }
